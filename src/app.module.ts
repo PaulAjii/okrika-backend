04cf7dc7741b2abdf1b90ed61f18139c2911f6e1
PaulAjii <paulajii134@gmail.com>
2025-12-14 01:54:25 +0100

    feat: add docker-compose file to run docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,36 +15,20 @@ import { validate } from './config/env.validation';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      // useFactory: (config: ConfigService) => ({
-      //   type: 'postgres',
-      //   host: config.get<string>('database.host'),
-      //   port: config.get<number>('database.port'),
-      //   username: config.get<string>('database.username'),
-      //   password: config.get<string>('database.password'),
-      //   database: config.get<string>('database.name'),
-      //   autoLoadEntities: true,
-      //   synchronize: config.get<string>('NODE_ENV') === 'development' || true,
-      // }),
-      useFactory: (config: ConfigService) => {
-        const dbConfig = {
-          type: 'postgres' as const,
-          host: config.get<string>('database.host'),
-          port: config.get<number>('database.port'),
-          username: config.get<string>('database.username'),
-          password: config.get<string>('database.password'),
-          database: config.get<string>('database.name'),
-          autoLoadEntities: true,
-          synchronize: true,
-        };
-        console.log('TypeORM Config:', {
-          host: dbConfig.host,
-          port: dbConfig.port,
-          username: dbConfig.username,
-          password: dbConfig.password ? '***' : 'undefined',
-          database: dbConfig.database,
-        });
-        return dbConfig;
-      },
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('database.host'),
+        port: config.get<number>('database.port'),
+        username: config.get<string>('database.username'),
+        password: config.get<string>('database.password'),
+        database: config.get<string>('database.name'),
+        autoLoadEntities: true,
+        synchronize: config.get<string>('NODE_ENV') === 'development' || true,
+        ssl: false,
+        extra: {
+          connectionTimeoutMillis: 10000,
+        },
+      }),
     }),
   ],
   controllers: [],
