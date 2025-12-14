19bc0570b94444e401a3e4a6f89852990a43a5a1
Olukayode David Ayomide <davidolukayode123@gmail.com>
2025-12-14 03:50:08 +0100

    feat: add system message for user operations

--- src/common/constants/system-messages.constant.ts
+++ src/common/constants/system-messages.constant.ts
@@ -3,6 +3,12 @@ export const SYSTEM_MESSAGES = {
     STARTUP: 'Okrika server has started successfully on http://localhost:{0}',
     API_DOC_STARTUP:
       'API documentation is available at http://localhost:{0}/api/docs',
+    API_DOC_REFERENCE:
+      'API Scalar Reference is available at http://localhost:{0}/api/reference',
     DATABASE_STARTUP: 'Database connection established successfully',
+  },
+
+  USER: {
+    GET_ALL: 'All users fetched successfully',
   },
 };
