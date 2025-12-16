b0a98797aaadd0b4bc376e68f4651f479de23d85
PaulAjii <paulajii134@gmail.com>
2025-12-16 20:31:57 +0100

    feat: add swagger features

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOkResponse({ description: 'Welcome to our API service' })
+  @ApiOperation({ summary: 'Welcomes and gets API details' })
   welcome() {
     return this.appService.welcome();
   }
