e25a820f70675b624dc9bf626850a1480cae9a38
PaulAjii <paulajii134@gmail.com>
2025-12-16 20:30:02 +0100

    feat: add seller fields to the user entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -12,4 +12,17 @@ export class User extends BaseEntity {
 
   @Column({ type: 'enum', enum: UserRole, default: UserRole.BUYER })
   role: UserRole;
+
+  @Column({ type: 'varchar' })
+  first_name: string;
+
+  @Column({ type: 'varchar' })
+  last_name: string;
+
+  // SELLER DETAILS
+  @Column({ type: 'varchar', nullable: true })
+  store_name: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  store_description: string;
 }
