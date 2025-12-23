87715cd782a034842748d6373b41f5ece879feaa
PaulAjii <paulajii134@gmail.com>
2025-12-23 19:11:06 +0100

    feat: add Roles decorator to restrict access to findAll method for ADMIN role

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/common/constants/roles.enum';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles(UserRole.ADMIN)
   @Get()
   @ApiOkResponse({ description: 'All users fetched successfully' })
   @ApiOperation({ summary: 'Get all users' })
