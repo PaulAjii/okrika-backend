934d0c8147624648fd929ca0b6c2bb7538143acb
PaulAjii <paulajii134@gmail.com>
2025-12-23 17:11:05 +0100

    feat: create LoginDto and update RegisterDto with proper property names and ApiProperty decorators

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,23 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class RegisterDto {
-  @ApiProperty()
+export class LoginDto {
+  @ApiProperty({ description: 'User email address' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({ minLength: 6, description: 'Minimum length of 6 characters' })
   @IsNotEmpty()
   @IsString()
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
   password: string;
-
-  @ApiProperty()
-  @IsString()
-  firstName: string;
-
-  @ApiProperty()
-  @IsString()
-  lastName: string;
 }
