0b668db303deeef79f0f319b974cece352ddf858
PaulAjii <paulajii134@gmail.com>
2025-12-23 19:10:43 +0100

    feat: implement RolesGuard for role-based access control

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,36 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { SYSTEM_MESSAGES } from '../constants/system-messages.constant';
+import { UserRole } from '../constants/roles.enum';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext) {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    const hasRole = requiredRoles.some((role) => user.role === role);
+
+    if (!hasRole) {
+      throw new ForbiddenException(SYSTEM_MESSAGES.AUTH.FORBIDDEN);
+    }
+
+    return true;
+  }
+}
